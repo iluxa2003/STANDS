584a44c7a9b2093c9eaa26d3c616e5c2c5d27f1b
Illia Yakovlev <illia.yakovlev@sigma.software>
2025-04-09 17:25:03 +0300

    function properties

---
+++ functions/funPropPractice.js
@@ -0,0 +1,15 @@
+// `function.name`, `function.length`, `arguments`, `caller`, `callee`
+
+// function name
+function smth() {}
+console.log(smth.name);
+
+function greet() {}
+console.log(greet.name);
+
+// function length
+function add(a, b, c) {}
+console.log(add.length); // 3
+
+function add1(a = "1234") {}
+console.log(add1.length); // 0
--- functions/thisPractice.js
+++ functions/thisPractice.js
@@ -1,6 +1,6 @@
 // Call Apply and Bind Practice
 
-//Call
+//Call - usefull if you need touse 1 func with multiple objects with similar properties
 const person = {
   fullname: function () {
     return this.firstname + ' ' + this.lastname;
@@ -12,7 +12,13 @@ const person1 = {
   lastname: 'Doe',
 };
 
+const person2 = {
+  firstname: 'Bob',
+  lastname: 'Glob',
+};
+
 console.log(person.fullname.call(person1));
+console.log(person.fullname.call(person2));
 
 //Apply
 const obj1 = {
@@ -25,9 +31,25 @@ let greeting = function (a, b) {
 
 console.log(greeting.apply(obj1, ['Hello', 'How are you? 1 func']));
 
+const numbers = [5, 8, 2, 10, 3];
+const maxNumber = Math.max.apply(null, numbers);
+console.log(maxNumber); // 10
+
 //Bind
 const obj2 = { name: 'John' };
 
 let bound = greeting.bind(obj2);
 
 console.log(bound('Hello', 'How are you? 2 func'));
+
+function multiply(a, b) {
+  return a * b;
+}
+
+const double = multiply.bind(null, 2); // First argument (a) is fixed to 2
+// const double = multiply(null, 2); error
+
+console.log(double(5)); // 10
+console.log(double(10)); // 20
+console.log(double); // function
+console.log(double()); // Nan
