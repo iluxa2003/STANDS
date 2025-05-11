1ebfb7346c0ccccb418325aae4844d4fba84a2b8
Illia Yakovlev <illia.yakovlev@sigma.software>
2025-05-11 14:59:04 +0300

    primitive types

---
+++ primitiveDataTypes/boolean.js
@@ -0,0 +1,15 @@
+// falsy
+// "" => false
+// 0 => false
+// NaN => false
+// 0n => false
+// undefined => false
+// null => false
+
+// truthy
+// [] => true
+// {} => true
+// "smth" => true
+// Symbol => true
+// 1 => true
+// 1n => true
