6767c71a13c831049c5d353a0c59cd098201debe
illiya <ilya.yakov.com@gmail.com>
2025-06-15 16:56:14 +0300

    obj3ect methods

--- objects/objectMethods.js
+++ objects/objectMethods.js
@@ -1,1 +1,22 @@
-const obj = {};
+const obj = {
+  name: 'Tiger',
+  type: 'predator',
+  environment: 'jungle',
+};
+
+Object.prototype.hasOwnProperty.call(obj, 'name');
+
+const values = Object.values(obj);
+console.log(values); // ["Tiger", "predator", "jungle"]
+
+const keys = Object.keys(obj);
+console.log(keys); // ["name", "type", "environment"]
+
+const entries = Object.entries(obj);
+console.log(entries); // [["name", "Tiger"], ["type", "predator"], ["environment", "jungle"]]
+
+const freeze = Object.freeze(obj);
+console.log(freeze); // dont let the object "change" (add new properties, remove properties, cant change properties, cant reconfigure)
+
+const assign = Object.assign(obj);
+console.log(assign);
